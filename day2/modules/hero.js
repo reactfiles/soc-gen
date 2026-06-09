a251942e131d027de72d0668a12f159d0f292653
React Lab Files <95652257+reactfiles@users.noreply.github.com>
2026-06-09 14:48:36 +0530

    Add files via upload

---
+++ day2/modules/hero.js
@@ -0,0 +1,29 @@
+import { Person } from "./person.js";
+
+class Hero extends Person {
+  title;
+  firstname;
+  lastname;
+  #mission = "Save the world";
+  static version = 1001;
+  constructor(fname, lname, title, hability) {
+    super(hability);
+    this.firstname = fname;
+    this.lastname = lname;
+    this.title = title;
+  }
+  fullname() {
+    return this.firstname + " " + this.lastname;
+  }
+  saymission() {
+    return this.#mission;
+  }
+  get accessMission() {
+    return this.#mission;
+  }
+  set accessMission(nmission) {
+    this.#mission = nmission;
+  }
+}
+
+export { Hero };
---
+++ day2/modules/person.js
@@ -0,0 +1,8 @@
+class Person {
+  ability = "i can walk";
+  constructor(pability) {
+    this.ability = pability;
+  }
+}
+// named export
+export { Person };
